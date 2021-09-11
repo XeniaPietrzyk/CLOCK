d7b7422b461f619abd9a4762ad40f59edfa8729c
Xenia Pietrzyk <xeniapietrzyk@gmail.com>
2021-09-11 18:32:32 +0200

    Scheme

---
+++ links.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "constants.h"
+#include "allmodels.h"
+#include "lodepng.h"
+#include "shaderprogram.h"
+#include "myCube.h"
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
---
+++ main_file.cpp
@@ -0,0 +1,59 @@
+/*
+Niniejszy program jest wolnym oprogramowaniem; możesz go
+rozprowadzać dalej i / lub modyfikować na warunkach Powszechnej
+Licencji Publicznej GNU, wydanej przez Fundację Wolnego
+Oprogramowania - według wersji 2 tej Licencji lub(według twojego
+wyboru) którejś z późniejszych wersji.
+
+Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on
+użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej
+gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH
+ZASTOSOWAŃ.W celu uzyskania bliższych informacji sięgnij do
+Powszechnej Licencji Publicznej GNU.
+
+Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
+Powszechnej Licencji Publicznej GNU(GNU General Public License);
+jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
+Place, Fifth Floor, Boston, MA  02110 - 1301  USA
+*/
+
+
+
+/* ZEGAR - PIETRZYK, SAMELAK*/
+
+#include "links.h"
+
+//STEP: Procedura obsługi błędów
+
+
+//STEP: Sterowanie
+
+
+//STEP: Wczytywanie modelu .obj
+
+
+//STEP: Wczytywanie tekstur
+
+
+//STEP: Procedura inicjująca
+
+
+//STEP: Zwolnienie zasobów zajętych przez program
+
+
+//STEP: Procedura rysująca zawartość sceny
+
+
+//STEP: Procedura rysująca obiekty
+
+
+int main(void)
+{
+
+	//NOTE: Inicjalizacja window
+	
+	//STEP: Główna pętla
+	
+	//STEP: Zwalnianie zasobów
+
+}
