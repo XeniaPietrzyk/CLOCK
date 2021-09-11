4ac7eeb9b343f5e113e78f92148fc6563d67af23
Xenia Pietrzyk <xeniapietrzyk@gmail.com>
2021-09-11 19:03:03 +0200

    Added main schema of the program

--- main_file.cpp
+++ main_file.cpp
@@ -17,43 +17,106 @@ jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
 Place, Fifth Floor, Boston, MA  02110 - 1301  USA
 */
 
-
-
 /* ZEGAR - PIETRZYK, SAMELAK*/
 
 #include "links.h"
 
 //STEP: Procedura obsługi błędów
-
+void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+}
 
 //STEP: Sterowanie
-
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod)
+{
+	//TODO: sterowanie
+}
 
 //STEP: Wczytywanie modelu .obj
-
+void loadModel(std::string plik) {
+	
+}
 
 //STEP: Wczytywanie tekstur
-
+GLuint readTexture(const char* filename) {
+	GLuint tex;
+	return tex;
+}
 
 //STEP: Procedura inicjująca
-
+void initOpenGLProgram(GLFWwindow* window) {
+	initShaders();	
+	glClearColor(0, 0, 0, 1); //Ustaw kolor czyszczenia bufora kolorów
+	glEnable(GL_DEPTH_TEST); //Włącz test głębokości na pikselach
+	glfwSetKeyCallback(window, key_callback);
+}
 
 //STEP: Zwolnienie zasobów zajętych przez program
-
+void freeOpenGLProgram(GLFWwindow* window) {
+	freeShaders();
+}
 
 //STEP: Procedura rysująca zawartość sceny
+void drawScene(GLFWwindow* window) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Wyczyść bufor koloru i bufor głębokości
 
+	//TODO: 
+
+	glfwSwapBuffers(window); //Skopiuj bufor tylny do bufora przedniego
+}
 
 //STEP: Procedura rysująca obiekty
+void drawObjects(GLFWwindow* window) {
 
+	//TODO:
+
+}
 
 int main(void)
 {
-
 	//NOTE: Inicjalizacja window
-	
-	//STEP: Główna pętla
-	
-	//STEP: Zwalnianie zasobów
+	GLFWwindow* window; //Wskaźnik na obiekt reprezentujący okno
 
+	glfwSetErrorCallback(error_callback);//Zarejestruj procedurę obsługi błędów
+
+	if (!glfwInit()) { //Zainicjuj bibliotekę GLFW
+		fprintf(stderr, "Nie można zainicjować GLFW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Utwórz okno 500x500 o tytule "OpenGL" i kontekst OpenGL.
+
+	if (!window) //Jeżeli okna nie udało się utworzyć, to zamknij program
+	{
+		fprintf(stderr, "Nie można utworzyć okna.\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window); //Od tego momentu kontekst okna staje się aktywny i polecenia OpenGL będą dotyczyć właśnie jego.
+	glfwSwapInterval(1); //Czekaj na 1 powrót plamki przed pokazaniem ukrytego bufora
+
+	if (glewInit() != GLEW_OK) { //Zainicjuj bibliotekę GLEW
+		fprintf(stderr, "Nie można zainicjować GLEW.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	initOpenGLProgram(window); //Operacje inicjujące
+
+
+	//STEP: Główna pętla	
+	while (!glfwWindowShouldClose(window)) //Tak długo jak okno nie powinno zostać zamknięte
+	{		
+		glfwSetTime(0); //Wyzeruj licznik czasu
+		drawScene(window); //Wykonaj procedurę rysującą
+		glfwPollEvents(); //Wykonaj procedury callback w zalezności od zdarzeń jakie zaszły.
+	}
+
+	freeOpenGLProgram(window);
+
+
+	//STEP: Zwalnianie zasobów
+	glfwDestroyWindow(window); //Usuń kontekst OpenGL i okno
+	glfwTerminate(); //Zwolnij zasoby zajęte przez GLFW
+	exit(EXIT_SUCCESS);
 }
