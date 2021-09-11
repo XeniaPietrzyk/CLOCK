e3045ec8beeefcf051a6b975d5482bf6e66d2269
Xenia Pietrzyk <xeniapietrzyk@gmail.com>
2021-09-11 19:23:40 +0200

    Added loadModel function

--- main_file.cpp
+++ main_file.cpp
@@ -26,6 +26,11 @@ using namespace std;
 
 GLuint tex;
 
+vector<vec4> verts;
+vector<vec4> norms;
+vector<vec2> texCoords;
+vector<unsigned int> indices;
+
 //STEP: Procedura obsługi błędów
 void error_callback(int error, const char* description) {
 	fputs(description, stderr);
@@ -37,13 +42,63 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	//TODO: sterowanie
 }
 
-//STEP: Wczytywanie modelu .obj
+//STEP: Procedura wczytywania modelu .obj
 void loadModel(std::string plik) {
 	
-	//TODO: wczytywanie modelu
+	using namespace std;
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(plik, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals);
+	cout << importer.GetErrorString() << endl;
+
+	aiMesh* mesh = scene->mMeshes[0];
+
+	for (int i = 0; i < mesh->mNumVertices; i++) {
+		aiVector3D vertex = mesh->mVertices[i]; //aiVector3D podobny do glm::vec3
+		verts.push_back(vec4(vertex.x, vertex.y, vertex.z, 1));
+
+		aiVector3D normal = mesh->mNormals[i]; //Wektory znormalizowane
+		norms.push_back(vec4(normal.x, normal.y, normal.z, 0));
+
+		/*
+		//liczba zdefiniowanych zestawów wsp. teksturowania (zestawów jest max 8)
+		unsigned int liczba_zest = mesh->GetNumColorChannels();
+		//liczba składowych wsp. teksturowania dla 0 zestawu
+		unsigned int wymiar_wsp_tex = mesh->mNumUVComponents[0];
+		*/
+		//0 to numer zestawu współrzędnych teksturowania
+		aiVector3D texCoord = mesh->mTextureCoords[0][i];
+		texCoords.push_back(vec2(texCoord.x, texCoord.y));
+		//x,y,z wykorzystywane jako u,v,w. 0 jeżeli tekstura ma mniej wymiarów
+	}
+
+	//dla każdego wielokąta składowego
+	for (int i = 0; i < mesh->mNumFaces; i++) {
+		aiFace& face = mesh->mFaces[i]; //face to jeden z wielokątów siatki
+
+		//dla każdego indeksu->wierzchołka tworzącego wielokąt
+		//dla aiProcess_Triangulate to zawsze będzie 3
+		for (int j = 0; j < face.mNumIndices; j++) {
+			indices.push_back(face.mIndices[j]);
+		}
+	}
+
+	aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+	
+
+	for (int i = 0; i < material->GetTextureCount(aiTextureType_DIFFUSE); i++) {
+		aiString str; //nazwa pliku
+
+		/*aiTextureMapping mapping; //jak wygenerowano np. teksturowania (opcj.)
+		unsigned int uvMapping; //numer zestawu wsp. teksturowania (opcjonalne)
+		ai_real blend; //współczynnik połązenia kolorów z kolejną teksturą (op.)
+		aiTextureOp op; //sposób łączenia kolorów z kolejną teksturą (opcjonalne)
+		aiTextureMapMode mapMode; //sposób adresowania tekstury (opcjonalne)*/
+		material->GetTexture(aiTextureType_DIFFUSE, i, &str); //, &mapping, &uvMapping, &blend, &op, &mapMode);
+		cout << str.C_Str() << endl;
+	}
 }
 
-//STEP: Wczytywanie tekstur
+//STEP: Procedura wczytywania tekstur
 GLuint readTexture(const char* filename) {
 	GLuint tex;
 	glActiveTexture(GL_TEXTURE0);
@@ -68,10 +123,9 @@ void initOpenGLProgram(GLFWwindow* window) {
 	glClearColor(0, 0, 0, 1); //Ustaw kolor czyszczenia bufora kolorów
 	glEnable(GL_DEPTH_TEST); //Włącz test głębokości na pikselach
 	glfwSetKeyCallback(window, key_callback);
-	tex = readTexture("bricks.png");
 }
 
-//STEP: Zwolnienie zasobów zajętych przez program
+//STEP: Procedura zwalniania zasobów zajętych przez program
 void freeOpenGLProgram(GLFWwindow* window) {
 	freeShaders();
 	glDeleteTextures(1, &tex);
@@ -81,7 +135,7 @@ void freeOpenGLProgram(GLFWwindow* window) {
 void drawScene(GLFWwindow* window) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Wyczyść bufor koloru i bufor głębokości
 
-	//TODO: 
+	//TODO: rysowanie sceny
 
 	glfwSwapBuffers(window); //Skopiuj bufor tylny do bufora przedniego
 }
@@ -89,7 +143,7 @@ void drawScene(GLFWwindow* window) {
 //STEP: Procedura rysująca obiekty
 void drawObjects(GLFWwindow* window) {
 
-	//TODO:
+	//TODO: rysowanie obiektu
 
 }
 
@@ -136,7 +190,7 @@ int main(void)
 	freeOpenGLProgram(window);
 
 
-	//STEP: Zwalnianie zasobów
+	//NOTE: Zwalnianie zasobów
 	glfwDestroyWindow(window); //Usuń kontekst OpenGL i okno
 	glfwTerminate(); //Zwolnij zasoby zajęte przez GLFW
 	exit(EXIT_SUCCESS);
